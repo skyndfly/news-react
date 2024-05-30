7a334b4b8f67a1aeabbcc49e84fd3d7dc9f789cc
Sergey Golubev <skyandy2796@yandex.ru>
2024-05-30 23:31:05 +0300

    refactor(pagination latest news): add wrapper for pagination and add new fetch for latest news

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -1,7 +1,7 @@
 import cls from './LatestNews.module.css';
 import BannersList from "../BannersList/BannersList.jsx";
 import {useFetch} from "../../helpers/hooks/useFetch.js";
-import {getLatestNews, getNews} from "../../api/apiNews.js";
+import {getLatestNews} from "../../api/apiNews.js";
 
 const LatestNews = () => {
     const {data, isLoading, error} = useFetch(getLatestNews);
